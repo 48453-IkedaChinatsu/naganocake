ed077d676cb43847307aeec5e356e37ba7f70a46
chinatsu <i.chinatsu0274@gmail.com>
2023-02-20 13:45:10 +0000

    first commit

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -5,7 +5,28 @@ class Admin::ItemsController < ApplicationController
     end
     
     def index
-    
+     @item = Item.new
+     @items = Item.all
     end
+    
+    def show
+    @item = Item.find(params[:id])
+    end
+    
+    def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_items_path(@item)
+    else
+      flash[:genre_created_error] = "ジャンル名を入力してください"
+    redirect_to new_admin_items_path
+    end
+    end
+    
+    private
+     # ストロングパラメータ
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :introduction, :price, :image, :is_active)
+  end
     
 end
