a8c984361efb0ddfa54d18230a53cc6590eea3b9
chinatsu <i.chinatsu0274@gmail.com>
2023-03-14 13:00:47 +0000

    first commit

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -87,20 +87,8 @@ class Public::OrdersController < ApplicationController
   def show
       @order = Order.find(params[:id])
       @order_details = @order.order_details
-   if @order.save
-	  cart_items.each do |cart|
-	  order_item = OrderItem.new
-	  order_item.item_id = cart.item_id
-      order_item.order_id = @order.id
-      order_item.order_quantity = cart.quantity
-      order_item.save
-   end
-      redirect_to 遷移したいページのパス
-      cart_items.destroy_all
-   else
-      @order = Order.new(order_params)
-      render :new
-   end
+      @order.shipping_cost = 800
+      @sum = 0
   end
 
   
